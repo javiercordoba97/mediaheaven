42ae25cb767e3ef545a063fba202203677d56f35
javiercordoba97 <cordobavjavier@gmail.com>
2025-02-02 15:59:20 -0300

    domingo prueba 1

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "development": {
-    "username": "proyecto2",
-    "password": "proyectodigitalhouse",
-    "database": "proyecto2_digital",
-    "host": "mysql-proyecto2.alwaysdata.net",
-    "dialect": "mysql"
+  development: {
+    username: 'proyecto2',
+    password: 'proyectodigitalhouse',
+    database: 'proyecto2_digital',
+    host: 'mysql-proyecto2.alwaysdata.net',
+    dialect: 'mysql'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   }
-}
+};
